5e80b8f63150d2d36507652e803c834f9d1f2133
hurleyquin9111 <hurleyquin9111@gmail.com>
2024-06-18 15:33:34 +0700

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <div>Sudah kah kalian makan hari ini ? </div>
     </main>
   );
