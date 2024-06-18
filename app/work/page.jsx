268863df3dc0d024114eda787905712e77faa33d
hurleyquin9111 <hurleyquin9111@gmail.com>
2024-06-18 16:25:48 +0700

    changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -163,12 +163,6 @@ const Work = () => {
                       <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                       {/*Bagian Image*/}
                       <div className=" relative w-full h-full">
-                        <img
-                          src={project.image}
-                          fill
-                          className="object-cover"
-                          alt=""
-                        />
                       </div>
                     </div>
                   </SwiperSlide>
