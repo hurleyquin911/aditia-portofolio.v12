d46b83ddaa76dadbc30a44068463433c6bcf22fa
hurleyquin9111 <hurleyquin9111@gmail.com>
2024-06-20 17:46:03 +0700

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/aditia-portofolio.v12" : "",
-  assetPrefix: isProd ? "/aditia-portofolio.v12/" : "",
   images: {
     unoptimized: true,
   },
